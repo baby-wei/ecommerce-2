bcd18322c0c82ddee4f5c9a05d723d168deb21d9
babywei <babywei@gmail.com>
2017-08-26 22:56:31 +0800

    48 Ajax Part 2

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 from django.views.generic.detail import SingleObjectMixin
@@ -37,7 +38,8 @@ class CartView(SingleObjectMixin, View):
 
 		# grab query on url and create cartitem accordingly
 		item_id = request.GET.get("item")
-		delete_item = request.GET.get("delete")
+		delete_item = request.GET.get("delete", False)
+		item_added = False
 		if item_id:
 			item_instance = get_object_or_404(Variation, id=item_id)
 			# 2nd arg gives qty a default qty of 1
@@ -47,12 +49,35 @@ class CartView(SingleObjectMixin, View):
 					delete_item = True
 			except:
 				raise Http404
-			cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
+			cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item_instance)
+			if created:
+				item_added = True
 			if delete_item:
 				cart_item.delete()
 			else:
 				cart_item.quantity = qty
 				cart_item.save()
+			if not request.is_ajax():
+				return HttpResponseRedirect(reverse("cart"))
+				# return cart_item.cart.get_absolute_url()
+
+		if request.is_ajax():
+			try:
+				total = cart_item.line_item_total
+			except:
+				total = None
+			try:
+				subtotal = cart_item.cart.subtotal
+			except:
+				subtotal = None
+			data = {
+					"deleted": delete_item, 
+					"item_added": item_added,
+					"line_total": total,
+					"subtotal": subtotal,
+					}
+			return JsonResponse(data)
+
 
 		context = {
 			"object": self.get_object()
